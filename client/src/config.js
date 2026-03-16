63bec9fe00f53bba01cc955d2acee66abb056fa9
shobhit2310 <shobhitkumar10110@gmail.com>
2026-03-16 12:31:13 +0530

    fix: dynamically fallback API_URL to current window origin to prevent 307 CORS Redirect failing on POST requests

--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,8 @@
 // Production deployment config
 // config file for API and Google auth
 export const API_URL =
-    process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+    process.env.REACT_APP_API_URL || 
+    (window.location.hostname === "localhost" ? "http://localhost:5000/api" : `${window.location.origin}/api`);
 
 export const GOOGLE_CLIENT_ID =
     process.env.REACT_APP_GOOGLE_CLIENT_ID || "565517695784-cvrrs1sd8jci61bdqjflah13kicnpj6t.apps.googleusercontent.com";
